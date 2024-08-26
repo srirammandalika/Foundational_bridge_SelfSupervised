45ca0db3da5c9b7ebc24e5674cae08523f38dd18
Sriram Mandalika <95198974+srirammandalika@users.noreply.github.com>
2024-08-26 15:23:29 +0530

    Update image_generation_dataset.py

--- datasets/image_generation_dataset.py
+++ datasets/image_generation_dataset.py
@@ -1,20 +1,21 @@
-# models/image_generation_net.py
-import numpy as np
-import torch
+# datasets/image_generation_dataset.py
+
 from torch.utils.data import Dataset
 
 class ImageGenerationDataset(Dataset):
-    def __init__(self, dataset, transform=None):
-        self.dataset = dataset
+    def __init__(self, data, transform=None):
+        self.images = data['train_images']
+        self.labels = data['train_labels']
         self.transform = transform
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        image, _ = self.dataset[idx]
+        image = self.images[idx]
 
         if self.transform:
-            image = self.transform(image)
+            im_q = self.transform(image)
+            im_k = self.transform(image)
 
-        return image, image
+        return (im_q, im_k)
